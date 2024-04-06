fe4f2c51e9d89752c6767e8172e3e892bf690d7f
Susucorn <127116788+Susucorn@users.noreply.github.com>
2024-04-06 18:38:35 +0900

    Add files via upload

---
+++ DataTypeConversion1.cs
@@ -0,0 +1,25 @@
+﻿/* Park Suyeon
+* <자료형 변환>
+* 설명 : 정수형 변환, 연산 결과 출력
+*/
+internal class DataTypeConversion1
+{
+    private static void Main(string[] args)
+    {
+        // 문자열로 입력 받은 후
+        Console.WriteLine("정수 두 개를 입력 : ");
+        string stringNum1 = Console.ReadLine();
+        string stringNum2 = Console.ReadLine();
+
+        // int 변환하기
+        int num1 = int.Parse(stringNum1);
+        int num2 = int.Parse(stringNum2);
+
+        // 출력하기
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+        Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+        Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+        Console.WriteLine(num1 + " / " + num2 + " = " + ((float)num1 / num2).ToString("F2"));
+        Console.WriteLine(num1 + " % " + num2 + " = " + (num1 % num2));
+    }
+}
---
+++ DataTypeConversion2.cs
@@ -0,0 +1,22 @@
+﻿/* Park Suyeon
+* <자료형 변환>
+* 설명 : 간단하게 문자열로 변환하기
+*/
+internal class DataTypeConversion2
+{
+    private static void Main(string[] args)
+    {
+        // 숫자와 빈 문자열을 더하면? --> 간단한 문자열 변환
+        int num = 13345;
+        String output = num + "";
+        Console.WriteLine(output);
+        Console.WriteLine("output 타입 : "+ output.GetType());
+
+        /*
+        char character = 'a';
+        String output2 = character;
+
+        --> ** 문자를 문자열로 변환이 불가능
+        */
+    }
+}
